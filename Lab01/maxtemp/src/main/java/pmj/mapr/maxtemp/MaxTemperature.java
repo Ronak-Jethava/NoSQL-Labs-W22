2c9af78fbbaa3f5d72ed31baac08ed25bc4790e8
PM Jat <pmjatz@gmail.com>
2022-01-27 11:39:32 +0530

    Add files via upload

--- Lab01/maxtemp/src/main/java/pmj/mapr/maxtemp/MaxTemperature.java
+++ Lab01/maxtemp/src/main/java/pmj/mapr/maxtemp/MaxTemperature.java
@@ -1,7 +1,5 @@
 package pmj.mapr.maxtemp;
 
-//cc MaxTemperature Application to find the maximum temperature in the weather dataset
-//vv MaxTemperature
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -11,29 +9,21 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MaxTemperature {
 
-public static void main(String[] args) throws Exception {
-	
- /*if (args.length != 2) {
-   System.err.println("Usage: MaxTemperature <input path> <output path>");
-   System.exit(-1);
- }*/
+	public static void main(String[] args) throws Exception {
  
- Job job = Job.getInstance();
- job.setJarByClass(MaxTemperature.class);
- job.setJobName("Max temperature");
+		Job job = Job.getInstance();
+		job.setJarByClass(MaxTemperature.class);
+		job.setJobName("Max temperature");
 
- FileInputFormat.addInputPath(job, new Path(args[0]));
- FileOutputFormat.setOutputPath(job, new Path(args[1]));
- //FileInputFormat.addInputPath(job, new Path("d:/programs20/nosql/maxtemp/tempdata.txt"));
- //FileOutputFormat.setOutputPath(job, new Path("d:/programs20/nosql/maxtemp/tempres"));
+		FileInputFormat.addInputPath(job, new Path("E:/programs-nosql-22/data/mr/weather"));
+		FileOutputFormat.setOutputPath(job, new Path("E:/programs-nosql-22/output/mr/temperature"));
  
- job.setMapperClass(MaxTemperatureMapper.class);
- job.setReducerClass(MaxTemperatureReducer.class);
+		job.setMapperClass(MaxTemperatureMapper.class);
+		job.setReducerClass(MaxTemperatureReducer.class);
 
- job.setOutputKeyClass(Text.class);
- job.setOutputValueClass(IntWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
  
- System.exit(job.waitForCompletion(true) ? 0 : 1);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
-}
-//^^ MaxTemperature
